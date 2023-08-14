72ffb7823755656f3e9f8967aea0cbb46aee744d
Lorenzo Gobbo <106741440+logo94@users.noreply.github.com>
2023-08-14 20:08:06 +0200

    Update oaiclient.py

--- include/oaiclient.py
+++ include/oaiclient.py
@@ -4,32 +4,37 @@ from oaipmh.metadata import MetadataRegistry, oai_dc_reader
 from csv import writer
 from time import sleep
 import json
-import string
 from include.tools import get_host, random_user, headerow, date
 import requests
 from bs4 import BeautifulSoup as bs
 
 def oai_count(url):
-    addr = url + '?verb=ListRecords&metadataPrefix=oai_dc'
-    page = requests.get(addr, headers=random_user())
-    if page.status_code != 200:
-        raise Exception()
-    soup = bs(page.text, 'xml')
-    count = soup.find('resumptionToken')['completeListSize']
-    return count
+    try:
+        addr = url + '?verb=ListRecords&metadataPrefix=oai_dc'
+        page = requests.get(addr, headers=random_user())
+        if page.status_code != 200:
+            raise Exception()
+        soup = bs(page.text, 'xml')
+        count = soup.find('resumptionToken')['completeListSize']
+        return count
+    except:
+        return '0'
 
 def oaiReq(url):
-    registry = MetadataRegistry()
-    registry.registerReader('oai_dc', oai_dc_reader)
-    client = Client(url, registry)
-    metadata = client.listRecords(metadataPrefix='oai_dc')
-    return metadata
+    try:
+        registry = MetadataRegistry()
+        registry.registerReader('oai_dc', oai_dc_reader)
+        client = Client(url, registry)
+        metadata = client.listRecords(metadataPrefix='oai_dc')
+        return metadata
+    except:
+        raise
 
 def popola(dc_field):
     rowen = []
     row_field = []
     for field in dc_field:
-        row_field.append(str(field).translate({ord(c): None for c in string.whitespace}))
+        row_field.append(str(field).replace('\n', '').replace('\t', '').replace('\r', ''))
     if len(row_field) > 0:
         if len(row_field) == 1:
             arrayo = row_field[0]
@@ -42,10 +47,10 @@ def popola(dc_field):
 def populate(n, dc_field):
     rowa = []
     if len(dc_field) > 0:
-        rowa.append(str(dc_field[0]).translate({ord(c): None for c in string.whitespace}))
+        rowa.append(str(dc_field[0]).replace('\n', '').replace('\t', '').replace('\r', ''))
         for i in range(1, n):
             try:
-                rowa.append(str(dc_field[i]).translate({ord(c): None for c in string.whitespace}))
+                rowa.append(str(dc_field[i]).replace('\n', '').replace('\t', '').replace('\r', ''))
             except: rowa.append('')
     else:
         for o in range(n): 
@@ -55,13 +60,16 @@ def populate(n, dc_field):
 ## TEST
 # Manda a schermo un solo record
 def test_oai(url):
-    limit = 0
-    while limit < 1:
-        for record in oaiReq(url):
-            element = record[1].getMap()
-            json_element = json.dumps(element, indent=4)
-            limit += 1
-            return json_element
+    try:
+        limit = 0
+        while limit < 1:
+            for record in oaiReq(url):
+                element = record[1].getMap()
+                json_element = json.dumps(element, indent=4)
+                limit += 1
+                return json_element
+    except:
+        return 'Set Dublin Core non disponibile'
 
 # Scrittura file CSV con colonne singole
 def csv_aggr_oai(url):
@@ -100,7 +108,7 @@ def csv_aggr_oai(url):
     except Exception as error:
         return 'Errore: ' + error
 
-    return 'File CSV salvato correttamente all\'interno della cartella metadata/oai/'
+    return 'File CSV salvato correttamente all\'interno della cartella metadata/OAI/'
 
 # Scrittura file CSV con colonne multiple
 def csv_multi_oai(url):
@@ -142,6 +150,6 @@ def csv_multi_oai(url):
     except Exception as error:
         return 'Errore: ' + error
 
-    return 'File CSV salvato correttamente all\'interno della cartella metadati/oai/'
+    return 'File CSV salvato correttamente all\'interno della cartella metadati/OAI/'
 
 
